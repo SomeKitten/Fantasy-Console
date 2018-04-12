14a7d2c45184a620332bfda7d7d2203363108e70
Kitten <blueredkitten@gmail.com>
2018-04-12 00:41:43 -0400

    Made code cleaner, and worked out problems/bugs related to displaying output.

---
+++ commands.lua
@@ -0,0 +1,134 @@
+commands = {}
+
+commands['PRINT'] = function(args)
+    out = termK.start(mathK.tabletostring(args))
+    
+    if ((out:sub(1, 1) == "'" and out:sub(-1, -1) == "'") or (out:sub(1, 1) == '"' and out:sub(-1, -1) == '"')) then
+      out = out:sub(2, -2)
+    end
+    
+    if out == nil then
+      termK.output(errorcodes[1])
+      return errorcodes[1]
+    else
+      termK.output(out)
+      return out
+    end
+end
+
+commands['CLS'] = function(args)
+    tempvars = {}
+    if #terminal.text > 0 then
+      for i = 1, #terminal.text do
+        terminal.text[i] = nil
+        terminal.cursor.x = 20
+        terminal.cursor.y = 20
+        terminal.top = 20
+      end
+      return "'" .. 'true' .. "'"
+    end
+    return "'" .. 'false' .. "'"
+end
+
+commands['SET'] = function(args)
+    local out = ''
+    for i = 2, #args do
+        out = out .. args[i] .. ' '
+      end
+      
+      out = out:sub(1, -2)
+      
+      tempvars[args[1]] = termK.start(out)
+      
+      return "'" .. out .. "'"
+end
+
+commands['HELP'] = function(args)
+    termK.output('COMMANDS ARE -- CLS   HELP   PRINT   SET')
+    return true
+end
+
+commands['LS'] = function(args)
+    local out = ''
+    for i,fname in ipairs(love.filesystem.getDirectoryItems( rootdir .. '/' .. curdir )) do
+      info = love.filesystem.getInfo( rootdir .. '/' .. curdir .. '/' .. fname )
+      if info.type == 'directory' then
+        out = out .. fname .. '/   '
+      else
+        out = out .. fname .. '   '
+      end
+    end
+    termK.output(out:sub(1,-4))
+end
+
+commands['MKDIR'] = function(args)
+    love.filesystem.createDirectory( rootdir .. '/' .. curdir .. '/' .. mathK.tabletostring(args) )
+end
+
+commands['MV'] = function(args)
+    local realdir = love.filesystem.getRealDirectory('')
+    if args[2] == nil then
+      love.filesystem.remove( rootdir .. '/' .. curdir .. args[1] )
+    else
+      os.rename(realdir .. '/' .. rootdir .. '/' .. curdir .. args[1], realdir .. '/' .. rootdir .. '/' .. curdir .. args[2])
+    end
+end
+
+commands['CD'] = function(args)
+    if mathK.tabletostring(args) ~= '..' then
+        info = love.filesystem.getInfo( rootdir .. '/' .. curdir .. mathK.tabletostring(args) )
+      
+        if info ~= nil then
+            if info.type == 'directory' then
+                curdir = curdir .. mathK.tabletostring(args) .. '/'
+            end
+        end
+    else
+        if curdir:sub(1,-2) ~= '/' then
+            curdir = curdir:sub(1,-2):sub(1, curdir:sub(1,-2):match'^.*()/')
+        end
+    end
+end
+
+commands['EDIT'] = function(args)
+    info = love.filesystem.getInfo( rootdir .. '/' .. curdir .. '/' .. mathK.tabletostring(args) )
+    if info == nil then
+      love.filesystem.write( rootdir .. '/' .. curdir .. '/' .. mathK.tabletostring(args), '' )
+      info = love.filesystem.getInfo( rootdir .. '/' .. curdir .. '/' .. mathK.tabletostring(args) )
+    end
+    
+    if info.type == 'file' then
+      inputBuff, _ = love.filesystem.read( rootdir .. '/' .. curdir .. '/' .. mathK.tabletostring(args) )
+      terminal.inputhistory[#terminal.inputhistory] = inputBuff
+      
+      termK.start('CLS')
+      
+      editingfile = mathK.tabletostring(args)
+      
+      mode = 'edit'
+    end
+end
+
+commands['INPUT'] = function(args)
+    oldmode = mode
+    mode = 'input'
+    writingto = mathK.tabletostring(args)
+    coroutine.yield(program)
+end
+
+DEFAULT_HANDLE = function(command, args)
+    local out = tempvars[mathK.tabletostring(mathK.tconcat({command}, args))]
+    
+    if out == nil then
+      if args == nil then
+        if command == nil then
+          return errorcodes[1]
+        end
+        return command
+      end
+      return mathK.tabletostring(mathK.tconcat({command}, args))
+    else
+      --termK.output(out)
+      return out
+    end
+end
--- termK.lua
+++ termK.lua
@@ -16,6 +16,7 @@ mode = 'terminal'
 oldmode = ''
 
 program = nil
+running = false
 
 enteringcommand = false
 
@@ -36,123 +37,11 @@ end
 function termK.evalcom (command, args)
   local out = ''
   
-  -- PRINT COMMAND
-  if command == 'PRINT' then
-    out = termK.start(mathK.tabletostring(args))
-    
-    if ((out:sub(1, 1) == "'" and out:sub(-1, -1) == "'") or (out:sub(1, 1) == '"' and out:sub(-1, -1) == '"')) then
-      out = out:sub(2, -2)
-    end
-    
-    if out == nil then
-      termK.output(errorcodes[1])
-      return errorcodes[1]
-    else
-      termK.output(out)
-      return out
-    end
-  elseif command == 'CLS' then
-    tempvars = {}
-    if #terminal.text > 0 then
-      for i = 1, #terminal.text do
-        terminal.text[i] = nil
-        terminal.cursor.x = 20
-        terminal.cursor.y = 20
-        terminal.top = 20
-      end
-      return "'" .. 'true' .. "'"
-    end
-    return "'" .. 'false' .. "'"
-  elseif command == 'SET' then
-    for i = 2, #args do
-      out = out .. args[i] .. ' '
-    end
-    
-    out = out:sub(1, -2)
-    
-    tempvars[args[1]] = termK.start(out)
-    
-    return "'" .. out .. "'"
-  elseif command == 'HELP' then
-    termK.output('COMMANDS ARE -- CLS   HELP   PRINT   SET')
-    return true
-  elseif command == 'LS' then
-    local out = ''
-    for i,fname in ipairs(love.filesystem.getDirectoryItems( rootdir .. '/' .. curdir )) do
-      info = love.filesystem.getInfo( rootdir .. '/' .. curdir .. '/' .. fname )
-      if info.type == 'directory' then
-        out = out .. fname .. '/   '
-      else
-        out = out .. fname .. '   '
-      end
-    end
-    termK.output(out:sub(1,-4))
-  elseif command == 'MKDIR' then
-    love.filesystem.createDirectory( rootdir .. '/' .. curdir .. '/' .. mathK.tabletostring(args) )
-  elseif command == 'MV' then
-    local realdir = love.filesystem.getRealDirectory('')
-    if args[2] == nil then
-      love.filesystem.remove( rootdir .. '/' .. curdir .. args[1] )
-    else
-      os.rename(realdir .. '/' .. rootdir .. '/' .. curdir .. args[1], realdir .. '/' .. rootdir .. '/' .. curdir .. args[2])
-    end
-  elseif command == 'CD' then
-    if mathK.tabletostring(args) ~= '..' then
-      info = love.filesystem.getInfo( rootdir .. '/' .. curdir .. mathK.tabletostring(args) )
-    
-      if info ~= nil then
-        if info.type == 'directory' then
-          curdir = curdir .. mathK.tabletostring(args) .. '/'
-        end
-      end
-    else
-      if curdir:sub(1,-2) ~= '/' then
-        curdir = curdir:sub(1,-2):sub(1, curdir:sub(1,-2):match'^.*()/')
-      end
-    end
-  elseif command == 'EDIT' then
-    info = love.filesystem.getInfo( rootdir .. '/' .. curdir .. '/' .. mathK.tabletostring(args) )
-    if info == nil then
-      love.filesystem.write( rootdir .. '/' .. curdir .. '/' .. mathK.tabletostring(args), '' )
-      info = love.filesystem.getInfo( rootdir .. '/' .. curdir .. '/' .. mathK.tabletostring(args) )
-    end
-    
-    if info.type == 'file' then
-      inputBuff, _ = love.filesystem.read( rootdir .. '/' .. curdir .. '/' .. mathK.tabletostring(args) )
-      terminal.inputhistory[#terminal.inputhistory] = inputBuff
-      
-      termK.start('CLS')
-      
-      editingfile = mathK.tabletostring(args)
-      
-      mode = 'edit'
-    end
-  elseif command == 'INPUT' then
-    if secondinputBuff == '' then
-      secondinputBuff = terminal.inputhistory[#terminal.inputhistory]
-      inputBuff = ''
-    end
-
-    oldmode = mode
-    mode = 'input'
-    writingto = mathK.tabletostring(args)
-    coroutine.yield(program)
-  else
-    local out = tempvars[mathK.tabletostring(mathK.tconcat({command}, args))]
-    
-    if out == nil then
-      if args == nil then
-        if command == nil then
-          return errorcodes[1]
-        end
-        return command
-      end
-      return mathK.tabletostring(mathK.tconcat({command}, args))
-    else
-      --termK.output(out)
-      return out
-    end
+  if commands[command] ~= nil then
+    return commands[command](args)
   end
+
+  return DEFAULT_HANDLE(command, args)
 end
 
 function termK.save()
@@ -182,8 +71,8 @@ function termK.input (t)
       termK.output('>' .. inputBuff)
       tempvars[writingto] = inputBuff
       mode = oldmode
-      inputBuff = secondinputBuff
-      terminal.inputhistory[#terminal.inputhistory + 1] = secondinputBuff
+      inputBuff = ''
+      terminal.inputhistory[#terminal.inputhistory + 1] = ''
 
       if program ~= nil then
         coroutine.resume(program)
@@ -197,11 +86,15 @@ function termK.input (t)
       elseif terminal.commands:sub(1,-2) == 's' then
         termK.save()
       elseif terminal.commands:sub(1,-2) == 'e' then
+        running = true
+
+        terminal.commands = ''
+        enteringcommand = false
+
         program = coroutine.create(function ()
           secondinputBuff = terminal.inputhistory[#terminal.inputhistory]
           terminal.inputhistory[#terminal.inputhistory] = ''
           inputBuff = ''
-          mode = 'running'
           termK.start('CLS')
   
           executablelines, _ = love.filesystem.read( rootdir .. '/' .. curdir .. '/' .. editingfile )
